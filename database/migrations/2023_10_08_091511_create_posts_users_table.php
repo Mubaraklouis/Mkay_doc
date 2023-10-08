adf9cfb283e23e431493800fd1eb74cad97b5ef4
Mubarak kual louis <mubaraklouis@gmail.com>
2023-10-08 12:21:08 +0300

    [task:2] make comments and pivot table migration

---
+++ database/migrations/2023_10_08_091511_create_posts_users_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('posts_users', function (Blueprint $table) {
+
+
+
+            $table->foreignId('user_id')->on('users')->reference('id')->casecadeOnDelete;
+            $table->foreignId('post_id')->on('posts')->reference('id')->casecadeOnDelete;
+
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('posts_users');
+    }
+};
